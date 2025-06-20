f29e0e0967032218940641239dbb9f3b0f23f894
Colin Goodall <cgoodall@GoodRiver.ca>
2025-06-20 15:26:30 -0400

    Branch001--Change003 - Added title bar to output

--- Wilma.cpp
+++ Wilma.cpp
@@ -7,6 +7,7 @@
 
 int  main( int argC, const char * argV[] )
 {
+	printf( "--==>Wilma<==--\n" );
 	for ( int nIndex = 0; nIndex < argC; nIndex++ )
 	{
 		printf( "argV[%d] -->%s<--\n", nIndex, argV[nIndex] );
